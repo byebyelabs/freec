23c4ad8dde40115507618f109c5232c88a0af945
Avaash Bhattarai <bhattara@grinnell.edu>
2026-04-29 20:05:22 -0500

    log file locations

--- src/tracing.c
+++ src/tracing.c
@@ -7,11 +7,13 @@
 #define TRACE_DEPTH 32
 
 void _fill_trace_info(alloc_node_t *node, trace_event_t event_type) {
+  log_message("[tracing]: _fill_trace_info called\n", DEBUG);
   if (node == NULL) {
-    perror("[tracing]: should be unreachable: node is null");
+    log_message("[tracing]: should be unreachable! node is `null`\n", ERROR);
+    exit(EXIT_FAILURE);
   }
 
-  node->last_event.event = event_type;
+  // node->last_event.event = event_type;
 
   // Citation:
   //    https://man7.org/linux/man-pages/man3/backtrace.3.html
@@ -19,6 +21,9 @@ void _fill_trace_info(alloc_node_t *node, trace_event_t event_type) {
   //    https://www.geeksforgeeks.org/c/pipe-system-call/
   void *traces[TRACE_DEPTH];
   size_t count = backtrace(traces, TRACE_DEPTH);
+
+  // backtrace_symbols makes a malloc call, which is why I added
+  // `CURRENTLY_IN_MALLOC` flag to avoid a recursive call
   char **funcNames = backtrace_symbols(traces, count);
 
   for (size_t i = 0; i < count; i++) {
@@ -27,7 +32,7 @@ void _fill_trace_info(alloc_node_t *node, trace_event_t event_type) {
     log_message(funcName, DEBUG);
   }
 
-  free(funcNames);
+  real_free(funcNames);
 }
 
 void _dump_error_info(alloc_node_t *node) {
