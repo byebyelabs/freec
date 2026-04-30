e661d447647afd2de361716b4b468d79d82a10d7
Avaash Bhattarai <bhattara@grinnell.edu>
2026-04-29 20:04:52 -0500

    need to know whether user is in the process of malloc to avoid recursive call

--- src/runtime.c
+++ src/runtime.c
@@ -1,8 +1,22 @@
 #include "utils.h"
 
+int USER_CURRENTLY_IN_MALLOC = 0;
+
 void *malloc(size_t size) {
+  if (USER_CURRENTLY_IN_MALLOC) {
+    // something made a recursive call to malloc.
+    // it must be our own code/libs we use. make real malloc call.
+    return real_malloc(size);
+  }
+
+  // malloc has started
+  USER_CURRENTLY_IN_MALLOC = 1;
+
   // TODO: malloc a page
   log_message("malloc called: malloc(zu)\n", DEBUG);
+
+  // malloc has finished
+  USER_CURRENTLY_IN_MALLOC = 0;
   return real_malloc(size);
 }
 
